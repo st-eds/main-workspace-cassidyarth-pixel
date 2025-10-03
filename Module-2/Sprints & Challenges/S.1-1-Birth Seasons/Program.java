d9fc5a91d1e437443a4c6ff8429c2e4dcf2bcdae
cassidyarth-pixel <cassidyarth@sehs.net>
2025-10-03 17:32:03 +0000

    Sprint 1

--- Module-2/Sprints & Challenges/S.1-1-Birth Seasons/Program.java
+++ Module-2/Sprints & Challenges/S.1-1-Birth Seasons/Program.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your birth month (1-12): ");
+        int month = input.nextInt();
+        input.nextLine();
+
+        if (month == 1 || month == 2 || month == 12) {
+            System.out.println("You're a winter baby!");
+        }
+        
+        else if (month == 3 || month == 4 || month == 5) {
+            System.out.println("You're a spring baby!");
+        }
+
+        else if (month == 6 || month == 7 || month == 8) {
+            System.out.println("You're a summer baby!");
+        }
+
+        else if (month == 9 || month == 10 || month == 11) {
+            System.out.println("You're a fall baby!");
+        }
+
+        else {
+            System.out.println("Error!");
+        }
     }
 }

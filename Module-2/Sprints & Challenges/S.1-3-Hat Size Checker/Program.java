a863a3710b491854bdfb798066be4221ba169d1f
cassidyarth-pixel <cassidyarth@sehs.net>
2025-10-09 02:43:37 +0000

    Sprint 2.3

--- Module-2/Sprints & Challenges/S.1-3-Hat Size Checker/Program.java
+++ Module-2/Sprints & Challenges/S.1-3-Hat Size Checker/Program.java
@@ -5,7 +5,31 @@ public class Program {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter your head size in inches: ");
-        int hatSize = input.nextInt
+        int hatSize = input.nextInt();
+
+        if (hatSize <= 21) {
+            System.out.println("Hat size: Small");
+            System.out.println("Snug fit for speedy thinkers");
+        }
+
+        else if (hatSize > 21 && hatSize <= 22.5) {
+            System.out.println("Hat size: Medium");
+            System.out.println("Balanced and brilliant.");
+        }
+
+        else if (hatSize > 22.5 && hatSize <= 24) {
+            System.out.println("Hat size: Large");
+            System.out.println("Roomy for big ideas.");
+        }
+
+        else if (hatSize > 24 && hatSize <= 30) {
+            System.out.println("Hat size: Extra Large");
+            System.out.println("Legendary noggin detected.");
+        }
+
+        else {
+            System.out.println("Dishonest hat haver spotted.");
+        }
         
     }
 }

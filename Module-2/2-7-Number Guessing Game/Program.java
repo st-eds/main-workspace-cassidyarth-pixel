8b40e01ec39e95274f6fcd9eccca215cd7dcdd47
cassidyarth-pixel <cassidyarth@sehs.net>
2025-10-21 14:09:41 +0000

    grer

---
+++ Module-2/2-7-Number Guessing Game/Program.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k = 10; while (k > 0) {System.out.println(k); k -= 2; }
+    }
+}

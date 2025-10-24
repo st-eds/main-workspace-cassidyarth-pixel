077fe98d4658b704e1d36a3cdbd7a081d05c68e4
cassidyarth-pixel <cassidyarth@sehs.net>
2025-10-24 13:07:30 +0000

    coin flep

---
+++ Fun/Tic Tac Toe/Program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 1; j++) {
+                System.out.print("\t|");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
--- Module-2/Sprints & Challenges/A.2-5-Heads or Tails/Program.java
+++ Module-2/Sprints & Challenges/A.2-5-Heads or Tails/Program.java
@@ -4,22 +4,30 @@ public class Program {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("-~- HEADS OR TAILS -~-\n");
+        System.out.println("-~- HEADS OR TAILS -~-");
 
-        boolean isPlaying = true
-        boolean userHeads = true
-        boolean compHeads = true
+        String userInput;
+        
+        boolean isPlaying = true;
+        boolean userHeads = true;
+        boolean compHeads = true;
+
+        int userWins = 0;
+        int userLosses = 0;
+        int rng = 0;
 
         while (isPlaying) {
-            System.out.print("Guess the outcome (heads/tails): ");
+            System.out.print("\nGuess the outcome (heads/tails): ");
+
+            userInput = input.nextLine().toLowerCase();
 
             // User Guess
-            if ((input.nextLine().toLowerCase()).equals("heads")) {
-                userHeads = true
+            if (userInput.equals("heads")) {
+                userHeads = true;
             }
 
-            else if ((input.nextLine().toLowerCase()).equals("tails")) {
-                userHeads = false
+            else if (userInput.equals("tails")) {
+                userHeads = false;
             }
 
             else {
@@ -27,14 +35,50 @@ public class Program {
                 System.exit(0);
             }
 
-            // True Outcome
-            if ((int)Math.random() == 0) {
-                compHeads = true
+
+            rng = (int)(Math.random() * 2) + 1;
+            System.out.println(rng);
+
+            // True Coin Sign
+            if (rng == 1) {
+                compHeads = true;
             }
 
-            else {
-                compHeads = false
+            else if (rng == 2) {
+                compHeads = false;
             }
+
+
+            // Reveal Coin Sign
+            if (compHeads) {
+                System.out.print("It's Heads! ");
+            }
+            
+            else {
+                System.out.print("It's Tails! ");
+            }
+
+            // Win/lose text & Score update
+            if (userHeads != compHeads) {
+                System.out.println("You Lose.");
+                userWins++;
+            }
+
+            else if (userHeads == compHeads) {
+                System.out.println("You Win!");
+                userLosses++;
+            }
+
+            System.out.println("Wins: " + userWins + " | Losses: " + userLosses);
+            System.out.print("\nPlay again? (y/n) ");
+
+            userInput = input.nextLine().toLowerCase();
+
+            if (userInput.equals("n") || userInput.equals("no")) {
+                isPlaying = false;
+                System.out.println("\nTHANK YOU FOR PLAYING!");
+            }
+
         }
     }
 }
